65b6941e81c25c6e1ceb70b3c6ca9ff1f849a891
PabloTzy <135358752+PabloTzy@users.noreply.github.com>
2024-04-25 13:43:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.write(unescape('%3Ciframe%20src%3D%22https%3A//animedex.pablobotz110.repl.co%22%20allowfullscreen%3E%3C/iframe%3E'))
+document.write(unescape('%3Ciframe%20src%3D%22https%3A//anime-indo.lol/%22%20allowfullscreen%3E%3C/iframe%3E'))
